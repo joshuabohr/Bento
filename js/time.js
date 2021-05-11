1c97813642ed80142cf4e27d930be772e881391e
joshuabohr <77680964+joshuabohr@users.noreply.github.com>
2021-05-11 15:09:44 +0200

    Update time.js

--- js/time.js
+++ js/time.js
@@ -1,18 +1,18 @@
 window.onload = displayClock();
 function displayClock() {
   const monthNames = [
-    'Jan',
-    'Feb',
-    'Mar',
-    'Apr',
-    'May',
-    'Jun',
-    'Jul',
-    'Aug',
-    'Sep',
-    'Oct',
-    'Nov',
-    'Dec',
+    'Gennaio',
+    'Febbraio',
+    'Marzo',
+    'Aprile',
+    'Maggio',
+    'Giugno',
+    'Luglio',
+    'Agosto',
+    'Settembre',
+    'Ottobre',
+    'Novembre',
+    'Dicembre',
   ];
 
   // Set to true to use a 12 hour date format
